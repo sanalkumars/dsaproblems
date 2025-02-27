45dbae9f8aa8aca66a21b3270cbf69e4d8ae8957
sanalkumars <sanalskumar126@gmail.com>
2025-02-27 16:13:25 +0530

    factorial using loop and recurssion

---
+++ factorial.js
@@ -0,0 +1,16 @@
+
+// using loop
+
+function facto(n) {
+    if (n<0) return "factorial of non-inter not defined"
+
+    if(n===0 || n===1) return "factorial is 1"
+    let res = 1;
+    for (let i = 2; i <= n; i++) {
+        res*=i
+        
+    }
+    return res
+}
+
+console.log(facto(5));
