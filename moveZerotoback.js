a127748bf5ee074233b33c1ce9dbb919f5a7fbbd
sanalkumars <sanalskumar126@gmail.com>
2025-02-27 15:37:34 +0530

    move zeros to the end of the array

---
+++ moveZerotoback.js
@@ -0,0 +1,19 @@
+
+
+function moveZeroToEnd(array) {
+    let nZ =0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== 0) {
+            [array[i],array[nZ]] = [ array[nZ],array[i]];
+            nZ++;
+        }
+        
+    }
+    return array
+}
+let array = [0,8,4,50,5,0,4,0]
+
+const result = moveZeroToEnd(array)
+
+console.log(result);
